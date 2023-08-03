380cab0142645a58e9bc024daeeed8e4b6369847
meliaramendoza <meliaramendoza@gmail.com>
2023-08-03 17:00:32 -0400

    Ejercicio P1

--- mysite/reserva/views.py
+++ mysite/reserva/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Reserva
 
-# Create your views here.
+def listar_reservas(request):
+    reservas = Reserva.objects.all()
+    return render(request, 'reserva/listar_reservas.html', {'reservas': reservas})
